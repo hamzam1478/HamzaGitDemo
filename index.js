199e23d5d3c794aae7385cc4204aa29a49084bb6
Hamza <hamzam1478@gmail.com>
2020-08-17 11:20:07 +0100

    Updated Index.js

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-let hamza = ("This is a test")
-console.log(hamza)
+class Person {
+    constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    }
+canVote() {
+    if (this.age > 17){
+        console.log("You are old enough to vote.")
+    }
+    else{
+        console.log("You are too young.")
+    }
+}
+}
+
+const Hamza = new Person ("Hamza", 18)
+console.log(Hamza)
+Hamza.canVote()
